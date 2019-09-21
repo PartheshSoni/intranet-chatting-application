a7ffcc4d18ea185494d9792ec3490bd4a79a22b5
Parthesh Soni <sonipartheshv@gmail.com>
2019-09-21 08:42:58 +0530

    Update DBConnect.java

--- src/intranet/chat/DBConnect.java
+++ src/intranet/chat/DBConnect.java
@@ -4,16 +4,7 @@ package intranet.chat;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Vishu
- */
 public class DBConnect {
     
             String url = "jdbc:mysql://localhost/alldata";
